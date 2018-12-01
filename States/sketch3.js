b54ebe4d1cd9667b858491d3f2426705d50e7364
彭津雲 <pxc1861@hotmail.com>
2018-12-01 19:51:12 +0800

    update state 3

---
+++ States/sketch3.js
@@ -0,0 +1,78 @@
+let rectArray = [];
+const PALETTE = ["#f35393","#DEC058","#f775f7","#2EC6F4","#f2f3f3"];
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noFill();
+    rectMode(CENTER);
+    for (let i = 0; i < max(int(windowWidth/20), int(windowHeight/20),40); i++) {
+        let singleRect = new BreathingRect();
+        rectArray.push(singleRect)
+
+    }
+}
+
+function draw() {
+    background(0);
+
+    rectArray.forEach(sr => {
+        sr.move();
+        sr.render();
+    })
+}
+
+function getRandomColor() {
+    const rando = floor(random(0,PALETTE.length));
+    return PALETTE[rando]
+}
+
+class BreathingRect {
+    constructor() {
+        this.position = createVector(random(width), random(height));
+        this.width = random(20, 200);
+        this.height = random(20, 200);
+
+        this.color = getRandomColor();
+        this.strokeWeight = random(1,5);
+        this.velocity = createVector(random(-5,5), random(-5,5));
+        this.velocity.normalize().mult(random(1,5));
+        this.randoRate = random(-3,3);
+        this.gen = floor(random(2));
+
+
+
+    }
+
+    move() {
+        if(this.position.x < width && this.position.x > 0 && this.position.y < height && this.position.y > 0 ) {
+            this.position.add(this.velocity);
+
+        } else if (this.position.x > width || this.position.x < 0) {
+            this.velocity.x *= -1.01;
+            this.position.add(this.velocity);
+        } else if (this.position.y > height || this.position.y < 0) {
+            this.velocity.y *= -1.01;
+            this.position.add(this.velocity);
+        }
+
+
+        this.width += this.velocity.x * this.randoRate;
+        this.height += this.velocity.y * this.randoRate;
+        this.strokeWeight = this.velocity.mag() * random(0.5,1);
+    }
+
+    render(){
+        stroke(this.color);
+        strokeWeight(this.strokeWeight);
+
+
+            rect(this.position.x, this.position.y, this.width, this.height);
+
+
+
+    }
+
+
+
+}
